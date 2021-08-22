b9fb4cf656245b037fb8d09c6c52fa77f8a2b6c5
Felipe Fernandes <felipe.fernandes@gam.com.br>
2021-08-22 20:59:21 -0300

    feat: :sparkles: Created first page and add Analytics.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import * as ga from '../../lib/ga'
+import { useRouter } from 'next/dist/client/router'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
+
 export default MyApp
---
+++ src/pages/home.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const BoxLaunchs = styled.div`
+    padding-bottom: 3rem;
+`;
+
+export const Launch = styled.div`
+    padding: 3rem 0;
+    font-family: 'SF UI Text Medium';
+
+    .name {
+        font-size: 1.75rem;
+        font-family: 'SF UI Text Medium';
+        margin-bottom: .5rem;
+    }
+
+    p {
+        margin: 0;
+    }
+`;
