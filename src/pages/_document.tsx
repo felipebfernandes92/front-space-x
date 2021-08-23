8ec1213dd00ea70c448f6c4cc1c4ae8be93a1b10
Felipe Fernandes <felipe.fernandes@gam.com.br>
2021-08-22 21:36:07 -0300

    chore: :chart_with_upwards_trend: Test code optimize.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,19 +12,13 @@ export default class MyDocument extends Document {
           />
           <script
             dangerouslySetInnerHTML={{
-              __html: `
-            (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-            h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-            (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-            })(window,document.documentElement,'async-hide','dataLayer',4000,
-            {'CONTAINER_ID':true});
-              
+              __html: `          
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-QSV8VPLV71', {
               page_path: window.location.pathname,
-              optimize_id: 'OPT-NQJXHDJ'
+              'optimize_id': 'OPT-NQJXHDJ'
             });
           `,
             }}
